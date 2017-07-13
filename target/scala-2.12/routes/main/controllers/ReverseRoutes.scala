e77ede95c1e38bee6a3af620db93f7b55c27d64a
Daniel Alejandro Florez Ospina <daniel@fearlessmonkey.tk>
2017-07-12 22:29:40 -0500

    Initial Commit

---
+++ target/scala-2.12/routes/main/controllers/ReverseRoutes.scala
@@ -0,0 +1,46 @@
+
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/home/daniel/Descargas/ncs-music/conf/routes
+// @DATE:Wed Jul 12 22:26:40 COT 2017
+
+import play.api.mvc.Call
+
+
+import _root_.controllers.Assets.Asset
+import _root_.play.libs.F
+
+// @LINE:6
+package controllers {
+
+  // @LINE:6
+  class ReverseHomeController(_prefix: => String) {
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:6
+    def index(): Call = {
+      
+      Call("GET", _prefix)
+    }
+  
+  }
+
+  // @LINE:9
+  class ReverseAssets(_prefix: => String) {
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:9
+    def versioned(file:Asset): Call = {
+      implicit val _rrc = new play.core.routing.ReverseRouteContext(Map(("path", "/public")))
+      Call("GET", _prefix + { _defaultPrefix } + "assets/" + implicitly[play.api.mvc.PathBindable[Asset]].unbind("file", file))
+    }
+  
+  }
+
+
+}
