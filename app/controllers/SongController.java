b4f04db86a91f81dae2d41c78a325f0575412152
Daniel Alejandro Florez Ospina <daniel@fearlessmonkey.tk>
2017-07-13 14:44:03 -0500

    add routing controllers
    added AccountController.java that will manage account things
    added SongController.java that will manage song things
    added routing in conf/routes to match the controllers
    added a line that sets the jdbc url default to local

---
+++ app/controllers/SongController.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import play.mvc.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class SongController extends Controller {
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result index() {
+        return ok("mostrando index de canciones");
+    }
+    public Result show(int id){
+        return ok("crmostrando una cancion");
+    }
+    public Result destroy(int id){
+        return ok("crmostrando una cancion");
+    }
+
+    public Result create(String username){
+        return ok("estoy creando una cancion");
+    }
+
+    public Result store(String username){
+        return ok("estoy guardando una cancion");
+    }
+}
